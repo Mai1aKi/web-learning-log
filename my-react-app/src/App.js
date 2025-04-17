46187fb65827d6cd75468746ed3f1a4b02a703d4
Mike Zeng <18938909629@163.com>
2025-04-17 00:09:19 -0400

    Update

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,14 +2,17 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import CalculatorPage from './pages/CalculatorPage';
+import UnderConstructionPage from './pages/UnderConstructionPage';
 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route path="/" element={<UnderConstructionPage />} />
         <Route path="/calculator" element={<CalculatorPage />} />
+        <Route path="/under-construction" element={<UnderConstructionPage />} />
+        <Route path="/homepage" element={<Homepage />} />
       </Routes>
     </Router>
   )
---
+++ my-react-app/src/pages/UnderConstructionPage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function UnderConstructionPage() {
+return (
+    <div
+        style={{
+            height: '100vh',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            fontFamily: 'Chalkduster, fantasy',
+            backgroundColor: '#000',
+            color: '#fff',
+            textAlign: 'center',
+            padding: '20px'
+        }}
+    >
+        <h1 style={{ fontSize: '3rem', marginBottom: '1rem' }}> Page Under Construction</h1>
+    </div>
+);
+}
+
+export default UnderConstructionPage;
